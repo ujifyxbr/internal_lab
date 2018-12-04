94f393016970fbf1580e4230b5f6ecc0f1d45cd9
ArtyemTS <tsibenkov.artyem@gmail.com>
2018-12-04 12:45:13 +0300

    Добавлены переменные которые будут добавляться в БД. Созданы аксессоры.

--- src/main/java/learn/epam/mlhh/domain/PutDB.java
+++ src/main/java/learn/epam/mlhh/domain/PutDB.java
@@ -11,4 +11,52 @@ public class PutDB {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
+    private String name;
+    private int age;
+    private String male;
+    private  String region;
+    private int salary;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getMale() {
+        return male;
+    }
+
+    public void setMale(String male) {
+        this.male = male;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+
 }
