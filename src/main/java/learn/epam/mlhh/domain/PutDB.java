9147a21dc199a9568699c1ccaeb9b864334e680b
ArtyemTS <tsibenkov.artyem@gmail.com>
2018-12-04 12:36:14 +0300

    Создана сущность PutDB для работы с БД

---
+++ src/main/java/learn/epam/mlhh/domain/PutDB.java
@@ -0,0 +1,14 @@
+package learn.epam.mlhh.domain;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class PutDB {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+}
