140501ced195f075b216eae2a527869710cf34a8
ArtyemTS <tsibenkov.artyem@gmail.com>
2018-12-04 14:09:35 +0300

    Cвязал контроллер с репо. Реализовал выдачу информации из БД на страницк main

--- src/main/java/learn/epam/mlhh/controllers/SearchController.java
+++ src/main/java/learn/epam/mlhh/controllers/SearchController.java
@@ -1,5 +1,8 @@
 package learn.epam.mlhh.controllers;
 
+import learn.epam.mlhh.domain.PutDB;
+import learn.epam.mlhh.repos.PutBDRepos;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.Map;
 @Controller
 public class SearchController {
+    @Autowired
+    private PutBDRepos putBDRepos;
+
     @GetMapping("/search")
     public String greeting(
             @RequestParam(name="name", required=false, defaultValue="World") String name,
@@ -17,7 +23,9 @@ public class SearchController {
     }
     @GetMapping
     public String main(Map<String, Object> model) {
-        model.put("some", "hello, letsCode!");
+        Iterable<PutDB> putDBS = putBDRepos.findAll();
+
+        model.put("candidates", putDBS);
         return "main";
     }
 }
