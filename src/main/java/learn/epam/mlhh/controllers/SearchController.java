86da53f78e35369fd03ef0d35ba73366536503fa
ArtyemTS <tsibenkov.artyem@gmail.com>
2018-12-04 08:23:33 +0300

    На данном этапе создан контроллер для страницы поиска. Сама страница поиска с помощью мусташ. Страница отображается на localhost:8080/search

---
+++ src/main/java/learn/epam/mlhh/controllers/SearchController.java
@@ -0,0 +1,23 @@
+package learn.epam.mlhh.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.util.Map;
+@Controller
+public class SearchController {
+    @GetMapping("/search")
+    public String greeting(
+            @RequestParam(name="name", required=false, defaultValue="World") String name,
+            Map<String, Object> model
+    ) {
+        model.put("name", name);
+        return "search";
+    }
+//    @GetMapping
+//    public String main(Map<String, Object> model) {
+//        model.put("some", "hello, letsCode!");
+//        return "index";
+//    }
+}
