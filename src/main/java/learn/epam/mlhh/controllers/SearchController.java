dccdd9a65b24b866cdab5813fd25026d158bcd9d
ArtyemTS <tsibenkov.artyem@gmail.com>
2018-12-09 15:02:24 +0300

    Создана web страница с критериями поиска

--- src/main/java/learn/epam/mlhh/controllers/SearchController.java
+++ src/main/java/learn/epam/mlhh/controllers/SearchController.java
@@ -2,12 +2,22 @@ package learn.epam.mlhh.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Map;
 
 @Controller
 public class SearchController {
-    @GetMapping("/searchdb")
-    public String home(){
-        return "searchdb";
+
+    private String message = "SEARCH";
+
+
+    @GetMapping("/search")
+
+    public String home (Map < String, Object > model){
+        model.put("message", this.message);
+        return "search";
     }
 
 }
--- src/main/java/learn/epam/mlhh/repos/PutBDRepos.java
+++
@@ -1,7 +0,0 @@
-package learn.epam.mlhh.repos;
-
-import learn.epam.mlhh.domain.PutDB;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PutBDRepos extends CrudRepository<PutDB, Integer> {
-}
