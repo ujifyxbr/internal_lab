be1e8bbc383552ddd2619bb55e58b661e870c2e8
Надежда Шальнова <esNadezhda@mail.ru>
2018-12-14 14:21:44 +0300

    TASK-1 added data for database

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,4 @@
-INSERT INTO candidate(name, age) VALUES ('Vasily', 21), ('Petya', 1);
+INSERT INTO candidate(name, age, developer, experience, gender, keyword, region, salary) VALUES ('Vasiliy', 21, 'ios', 1, 'male', 'ios, swift, objective-c', 'Moscow', 50000),('Peter', 35, 'android', 10, 'male', 'android, java, git', 'NN', 250000) ;
+
+INSERT INTO users(user_name, user_password) VALUES ('Vasiliy','password');
+
